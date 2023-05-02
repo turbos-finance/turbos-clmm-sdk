22a79582232868983d2fadbd59c8394d1f167097
runcat <runcat1234567@gmail.com>
2023-05-02 10:27:18 +0800

    feat: export BN and Decimal modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { default as BN } from 'bn.js';
+export { default as Decimal } from 'decimal.js';
 export * from './sdk';
 export * from './constants';
 export { CreatePoolOptions, Fee, AddLiquidityOptions, PoolFields } from './lib';
