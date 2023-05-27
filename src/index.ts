743a8eb42596e517927e544e2c552eb296c98417
runcat <runcat1234567@gmail.com>
2023-05-27 16:47:50 +0800

    feat: export all constant variables

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as BN } from 'bn.js';
 export { default as Decimal } from 'decimal.js';
-export { Network } from './constants';
+export * from './constants';
 export * from './sdk';
 export * from './lib';
