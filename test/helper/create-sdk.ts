8e81a254fd1d22e864b64b5bd8c842085d4afb01
runcat <runcat1234567@gmail.com>
2023-05-29 16:40:37 +0800

    fix: sdk constructor in test file

--- test/helper/create-sdk.ts
+++ test/helper/create-sdk.ts
@@ -2,4 +2,4 @@ import { JsonRpcProvider, testnetConnection } from '@mysten/sui.js';
 import { Network, TurbosSdk } from '../../src';
 
 export const createSdk = () =>
-  new TurbosSdk(new JsonRpcProvider(testnetConnection), Network.testnet);
+  new TurbosSdk(Network.testnet, new JsonRpcProvider(testnetConnection));
