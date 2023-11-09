399fa592e2d3fc8252673981707234c0e85a505f
blockJser@gmx.com <blockJser@gmx.com>
2023-11-09 11:46:03 +0800

    refactor: test

--- test/coin.test.ts
+++ test/coin.test.ts
@@ -2,7 +2,7 @@ import { createSdk } from './helper/create-sdk';
 
 const sdk = createSdk();
 
-test.each(['a::sui::SUI', 'a.sui', 'SUI', 'sui', 'suik'])(
+test.each(['a::sui::SUI', '0x2::sui::sui', '0x000000000000002::sui::sui'])(
   'valid sui literal',
   (value) => {
     expect(sdk.coin.isSUI(value)).toBeTruthy();
